4c4b3c4d53c26cd765bf3dd65294a37fec8a2399
Carolyn Chow <carolynchow@Carolyns-MacBook-Air.local>
2020-05-19 18:12:05 -0400

    editing success page

--- contact.php
+++ contact.php
@@ -7,5 +7,5 @@ $recipient = "carolynchow31@gmail.com";
 $subject = "Contact Form from CareForFitness";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo "Thank You!" . " -" . "<a href='successForm.html'> </a>;
 ?>
